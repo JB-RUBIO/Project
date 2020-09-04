d69232135692281096ad40151873083ec09582ee
JB <rubiojeanbaptiste@gmail.com>
2020-09-04 19:42:16 +0200

    first commit

---
+++ decorators.py
@@ -0,0 +1,17 @@
+# from classes import Flight
+
+
+def announce(f):
+    def wrapper():
+        print("About to run the function...")
+        f()
+        print("Done with the function.")
+    return wrapper
+
+
+@announce
+def hello():
+    print("Hello world!")
+
+
+hello()
---
+++ lambda.py
@@ -0,0 +1,10 @@
+people = [
+    {"name": "p1", "house": "h1"},
+    {"name": "p2", "house": "h2"},
+    {"name": "p3", "house": "h3"},
+    {"name": "p4", "house": "h4"},
+]
+
+people.sort()
+
+print(people)
